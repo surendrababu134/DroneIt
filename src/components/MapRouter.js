789622d5c3cd2742830bc0aba5f64935a5b7bf38
Surendra Babu <surendra134@gmail.com>
2019-07-06 13:32:37 +0530

    map path completed

--- src/components/MapRouter.js
+++ src/components/MapRouter.js
@@ -1,24 +1,97 @@
-import React, { Component } from 'react'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from 'react';
+import { Map, InfoWindow, Polygon, Polyline, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-class MapRouter extends Component {
+const mapStyles = {
+    width: '100%',
+    height: '100%'
+};
+
+
+export class MapRouter extends Component {
+    // initMap() {
+    //     let directionsDisplay = Map.DirectionsRenderer;
+    //     let directionsService = Map.DirectionsService;
+    //     let map = Map(document.getElementById('map'), {
+    //         zoom: 14,
+    //         center: { lat: 37.77, lng: -122.447 }
+    //     });
+    //     directionsDisplay.setMap(map);
+
+    //     this.calculateAndDisplayRoute(directionsService, directionsDisplay);
+    //     document.getElementById('mode').addEventListener('change', function () {
+    //         this.calculateAndDisplayRoute(directionsService, directionsDisplay);
+    //     });
+    // }
+    // calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    //     let selectedMode = document.getElementById('mode').value;
+    //     directionsService.route({
+    //         origin: { lat: 37.77, lng: -122.447 }, // Haight.
+    //         destination: { lat: 37.768, lng: -122.511 }, // Ocean Beach.
+    //         // Note that Javascript allows us to access the constant
+    //         // using square brackets and a string value as its
+    //         // "property."
+    //         travelMode: Map.TravelMode[selectedMode]
+    //     }, function (response, status) {
+    //         if (status == 'OK') {
+    //             directionsDisplay.setDirections(response);
+    //         } else {
+    //             window.alert('Directions request failed due to ' + status);
+    //         }
+    //     });
+    // }
     render() {
+        const triangleCoords = [
+            { lat: 17.440081,
+                lng: 78.348915 },
+            { lat: 17.440081,
+                lng: 78.448915 }
+        ];
+
         return (
-            <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            {/* <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div> */}
-        </InfoWindow>
-      </Map>
+            <Map google={this.props.google} initialCenter={{
+                lat: 17.440081,
+                lng: 78.348915
+            }}
+                style={{ width: '100%', height: '100%', position: 'relative' }}
+                className={'map'}
+                zoom={14}>
+                <Marker onClick={this.onMarkerClick}
+                    title={'The marker`s title will appear as a tooltip.'}
+                    name={'SOMA'}
+                    position={{ lat: 17.440081,
+                        lng: 78.348915 }} />
+                <Marker
+                    name={'Dolores park'}
+                    position={{ lat: 17.440081,
+                        lng: 78.448915 }} />
+                <Marker />
+                <Polyline
+                    path={triangleCoords}
+                    strokeColor="#0000FF"
+                    strokeOpacity={0.8}
+                    strokeWeight={2} />
+            </Map>
         )
     }
+    // render() {
+    //     return (
+    //         <div>
+    //             <Map
+    //                 google={this.props.google}
+    //                 zoom={14}
+    //                 style={mapStyles}
+    //                 initialCenter={{
+    //                     lat: -1.2884,
+    //                     lng: 36.8233
+    //                 }}
+    //             />
+
+    //         </div>
+
+    //     );
+    // }
 }
 
 export default GoogleApiWrapper({
-    apiKey: ("AIzaSyAhAZI1a2yA-r7HHVTko9yWIEOv4M3iS2M")
-  })(MapRouter)
+    apiKey: 'AIzaSyDI_i2I0JuB1wABeEsm0nQqPx4VuGlG09k'
+})(MapRouter);
